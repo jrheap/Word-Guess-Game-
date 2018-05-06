d4edee7b5fe1f7af3a211c8fb098c38fff588979
Jason <jasonrheap@gmial.com>
2018-05-05 20:26:12 -0600

    8:26

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,134 @@
-// Global variables
-var wordOptions = ["washington","california","florida","illinois","ohio","oregon","texas","michigan","colorado","georgia","missouri","pennsylvania","new york","kentucky","tennesse"];
-var selectedWord = "";
-var letterinWord = [];
-var numBlanks = 0;
-var blanksAndSuccesses = [];
-var wrongLetters = [];
-var winCount = 0;
-var lossCount = 0;
-var guessesLeft = 12;
-// Functions
-function startGame () {
-    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
-    // what is .split??
-    letterinWord = selectedWord.split("");
-    numBlanks = letterinWord.length;
+    // Array of words
+    var words = ["washington", "california", "florida", "illinois", "ohio", "oregon", "texas", "michigan", "colorado", "georiga", "missouri", "pennsylvania", "new york", "kentucky", "tennessee"];
 
-    guessesLeft = 12;
-    // why the blank []
-    wrongLetters = [];
-    blanksAndSuccesses = [];
+    var currentWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
+    console.log(currentWord);
 
-    // What the heck is going on here?? adds the _ _ _ _
-    for (var i=0; i<numBlanks; i++){
-        blanksAndSuccesses.push("_");
+    
+    var guessesLeft = 10;
+    document.getElementById("guesses-left").textContent = "Guesses Left: " + guessesLeft;
+
+    // wins
+    var wins = 0;
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    //losses
+    var loss = 0;
+    document.getElementById("losses").innerHTML = "Losses: " + loss;
+
+    var resetLettersGuessed = "Letters Guessed: "
+
+   
+    var progressWord = [];
+
+    var mysteryWord = [];
+    var i;
+ 
+    for (i = 0; i < currentWord.length; i++) {
+        progressWord.push("__");
+    }
+    document.getElementById("word-guess").innerHTML = progressWord.join(" ");
+
+    function letterInWord(letter) {
+        
+        var positions = new Array();
+        for (i = 0 ; i < currentWord.length; i++) {
+            if (currentWord[i] === letter)
+                positions.push(i);
+        }
+        return positions
     }
 
-    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses[" "];
+    
+    function lettersToGuess() {
+        var i ;
+        var toGess = 0 ;
+        for (i in progressWord) {
+            if (progressWord[i] === "__")
+                toGess++;
+        }
+        return toGess;
+    }
 
-    // great way to check your code
-console.log(selectedWord);
-console.log(letterinWord);
-console.log(numBlanks);
-console.log(blanksAndSuccesses);
-}
+    
+    document.onkeyup = function (event) {
+        var letter = event.key;
+        var lettersGuessed = letter.toLocaleUpperCase();
+        var i;
+console.log(letter)
 
-// Main process
-// calls the function
-startGame();
+        var positions = letterInWord(lettersGuessed);
+
+
+        
+        if (positions.length) {
+
+            for (i = 0 ; i < positions.length; i++) {
+                progressWord[positions[i]] = lettersGuessed;
+            }
+
+            
+            document.getElementById("word-guess").innerHTML = progressWord.join(" ");
+        } else {
+            document.getElementById("letters-guessed").innerHTML += lettersGuessed + " ";
+
+            
+            guessesLeft--;
+            document.getElementById("guesses-left").textContent = "Guesses Left: " + guessesLeft;
+        }
+
+
+        if (guessesLeft === 0) {
+   
+            
+            loss = loss + 1;
+            
+            document.getElementById("losses").innerHTML = "Losses: " + loss;
+            
+            guessesLeft = 10;
+            
+            document.getElementById("letters-guessed").innerHTML = resetLettersGuessed;
+            
+            currentWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
+            
+            progressWord = [];
+            for (i = 0; i < currentWord.length; i++) {
+            progressWord[positions[i]].push("__");
+        }
+
+
+
+        }
+
+        
+        if (lettersToGuess() == 0) {
+
+        	// alert("You Deserve a day on the beach!");
+            
+
+
+
+
+           //RESET
+            guessesLeft = 10;
+            document.getElementById("guesses-left").textContent = "Guesses Left: " + guessesLeft;
+
+           
+            document.getElementById("letters-guessed").innerHTML = resetLettersGuessed;
+
+            // NEW WORD
+            currentWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
+            console.log(currentWord)
+            
+
+            progressWord = [];
+            for (i = 0; i < currentWord.length; i++) {
+                progressWord.push("__");
+            }
+            document.getElementById("word-guess").innerHTML = progressWord.join(" ");
+
+            
+            wins++;
+            document.getElementById("wins").innerHTML = "Wins: " + wins;
+            
+        }
+    }
